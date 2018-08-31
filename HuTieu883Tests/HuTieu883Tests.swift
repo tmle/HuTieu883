d56f45584c935baf10a0b89de0416a41dd3a2dea
Thinh M. Le <thinh.m.le@gmail.com>
2018-08-31 08:29:33 -0700

    second commit

---
+++ HuTieu883Tests/HuTieu883Tests.swift
@@ -0,0 +1,36 @@
+//
+//  HuTieu883Tests.swift
+//  HuTieu883Tests
+//
+//  Created by Thinh Le on 2018-08-31.
+//  Copyright © 2018 Thinh Le. All rights reserved.
+//
+
+import XCTest
+@testable import HuTieu883
+
+class HuTieu883Tests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
