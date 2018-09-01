6068b5ce0e5f782b934832ee69bc0e4bb37af600
Thinh M. Le <thinh.m.le@gmail.com>
2018-08-31 17:52:12 -0700

    fast asynchronous table cell image loading

--- HuTieu883/Controller/ProductsTableViewController.swift
+++ HuTieu883/Controller/ProductsTableViewController.swift
@@ -15,9 +15,12 @@ class ProductsTableViewController: UITableViewController {
     let PRODUCT_URL = "https://www.thinhmle.com/api/ProductList_2_skintree.json"
     let params : [String : String] = ["userId" : "userId", "password" : "password"]
     var products: Array = [ProductModel]()
+    let pendingOperations = PendingOperations()
+    let start = DispatchTime.now() // <<<<<<<<<< Start time
 
     override func viewDidLoad() {
         super.viewDidLoad()
+        title = "List of Products"
 
         getProductData(url: PRODUCT_URL, parameters: params)
     }
@@ -62,15 +65,147 @@ class ProductsTableViewController: UITableViewController {
     override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         let cell = tableView.dequeueReusableCell(withIdentifier: "ProductCell", for: indexPath)
 
+        
+        //1
+        if cell.accessoryView == nil {
+            let indicator = UIActivityIndicatorView(activityIndicatorStyle: .gray)
+            cell.accessoryView = indicator
+        }
+        let indicator = cell.accessoryView as! UIActivityIndicatorView
+        
+        
+        //2
         let prod = products[indexPath.row]
+
+        //3
+        cell.imageView?.image = prod.thumbnail
         
-        let url = URL(string: prod.thumbnailURL)!
-        let thumbnailURLData = NSData(contentsOf: url as URL)
-        cell.imageView?.image = UIImage(data: thumbnailURLData! as Data)
-        cell.textLabel?.text = prod.name
-        cell.detailTextLabel?.text = prod.brief
-        
+        //4
+        switch (prod.state){
+        case .failed:
+            indicator.stopAnimating()
+            cell.textLabel?.text = "Failed to load"
+        case .downloaded:
+            indicator.stopAnimating()
+            cell.accessoryType = UITableViewCellAccessoryType.disclosureIndicator
+            cell.textLabel!.font = UIFont.systemFont(ofSize: 17.0)
+            cell.textLabel!.textColor = UIColor(red: 0.0, green: 0.004, blue: 0.502, alpha: 1.0)
+            cell.textLabel?.text = prod.name
+            cell.detailTextLabel!.font = UIFont.systemFont(ofSize: 15.0)
+            cell.detailTextLabel!.textColor = UIColor(red: 0.5, green: 0.004, blue: 0.502, alpha: 1.0)
+            cell.detailTextLabel?.text = prod.brief
+        case .new:
+            indicator.startAnimating()
+            if (!tableView.isDragging && !tableView.isDecelerating) {
+                self.startOperationsForProductModel(prod, indexPath: indexPath)
+            }
+        }
+
         return cell
+    }
+    
+    // MARK: - Asynchronous Image Downloading
+    func startOperationsForProductModel(_ product: ProductModel, indexPath: IndexPath){
+        switch (product.state) {
+        case .new:
+            startDownloadForProduct(product, indexPath: indexPath)
+            
+        case .downloaded:
+            let end = DispatchTime.now()   // <<<<<<<<<<   end time
+            
+            //    let theAnswer = self.checkAnswer(answerNum: "\(problemNumber)", guess: myGuess)
+            let nanoTime = end.uptimeNanoseconds - self.start.uptimeNanoseconds // <<<<< Difference in nano seconds (UInt64)
+            let timeInterval = Double(nanoTime) / 1_000_000_000 // Technically could overflow for long running tests
+            
+            print("Time to get first photo: \(timeInterval) seconds")
+            
+        default:
+            let _: String = ""
+        }
+    }
+    
+    func startDownloadForProduct(_ product: ProductModel, indexPath: IndexPath){
+        //1
+        if pendingOperations.downloadsInProgress[indexPath] != nil {
+            return
+        }
+        
+        //2
+        let downloader = ImageDownloader(productModel: product)
+        //3
+        downloader.completionBlock = {
+            if downloader.isCancelled {
+                return
+            }
+            DispatchQueue.main.async(execute: {
+                self.pendingOperations.downloadsInProgress.removeValue(forKey: indexPath)
+                self.tableView.reloadRows(at: [indexPath], with: .fade)
+            })
+        }
+        //4
+        pendingOperations.downloadsInProgress[indexPath] = downloader
+        //5
+        pendingOperations.downloadQueue.addOperation(downloader)
+    }
+    
+    // MARK: - Table Cell Operations
+    override func scrollViewWillBeginDragging(_ scrollView: UIScrollView) {
+        //1
+        suspendAllOperations()
+    }
+    
+    override func scrollViewDidEndDragging(_ scrollView: UIScrollView, willDecelerate decelerate: Bool) {
+        // 2
+        if !decelerate {
+            loadImagesForOnscreenCells()
+            resumeAllOperations()
+        }
+    }
+    
+    override func scrollViewDidEndDecelerating(_ scrollView: UIScrollView) {
+        // 3
+        loadImagesForOnscreenCells()
+        resumeAllOperations()
+    }
+    
+    func suspendAllOperations () {
+        pendingOperations.downloadQueue.isSuspended = true
+    }
+    
+    func resumeAllOperations () {
+        pendingOperations.downloadQueue.isSuspended = false
+    }
+    
+    func loadImagesForOnscreenCells () {
+        //1
+        if let pathsArray = tableView.indexPathsForVisibleRows {
+            //2
+            let allPendingOperations = Set(Array(pendingOperations.downloadsInProgress.keys))
+            
+            //3
+            var toBeCancelled = allPendingOperations
+            let visiblePaths = Set(pathsArray )
+            toBeCancelled.subtract(visiblePaths)
+            
+            //4
+            var toBeStarted = visiblePaths
+            toBeStarted.subtract(allPendingOperations)
+            
+            // 5
+            for indexPath in toBeCancelled {
+                if let pendingDownload = pendingOperations.downloadsInProgress[indexPath] {
+                    pendingDownload.cancel()
+                }
+                pendingOperations.downloadsInProgress.removeValue(forKey: indexPath)
+            }
+            
+            // 6
+            for indexPath in toBeStarted {
+                let indexPath = indexPath as IndexPath
+                let productToProcess = self.products[(indexPath as NSIndexPath).row]
+                startOperationsForProductModel(productToProcess, indexPath: indexPath)
+            }
+        }
     }
 
     /*
--- HuTieu883/Model/ProductModel.swift
+++ HuTieu883/Model/ProductModel.swift
@@ -7,14 +7,21 @@
 //
 
 import Foundation
+import UIKit
+
+enum ProductModelState {
+    case new, downloaded, failed
+}
+
 
 class ProductModel {
     var name: String = ""
     var brief: String = ""
     var thumbnailURL: String = "";
+    var state = ProductModelState.new
+    var thumbnail = UIImage(named: "camera-48x48.png")
     
     init() {
-        
     }
 
     func hasThumbnail(productThumbnailURL: String) -> Bool {
@@ -22,3 +29,52 @@ class ProductModel {
     }
 
 }
+
+class PendingOperations {
+    lazy var downloadsInProgress = [IndexPath:Operation]()
+    lazy var downloadQueue:OperationQueue = {
+        var queue = OperationQueue()
+        queue.name = "Download queue"
+        queue.maxConcurrentOperationCount = 1
+        return queue
+    }()
+}
+
+class ImageDownloader: Operation {
+    //1
+    let productModel: ProductModel
+    
+    //2
+    init(productModel: ProductModel) {
+        self.productModel = productModel
+    }
+    
+    //3
+    override func main() {
+        //4
+        if self.isCancelled {
+            return
+        }
+        //5
+        let currentUrl = URL(string: productModel.thumbnailURL)!
+        let imageData = try? Data(contentsOf: currentUrl)
+        
+        //6
+        if self.isCancelled {
+            return
+        }
+        
+        //7
+        if imageData != nil {
+            self.productModel.thumbnail = UIImage(data:imageData!)
+            self.productModel.state = .downloaded
+        }
+        else
+        {
+            self.productModel.state = .failed
+            self.productModel.thumbnail = UIImage(named: "not-avail-48x48.png")
+        }
+    }
+}
+
+
